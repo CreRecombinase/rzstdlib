fe4eb84d42c3259389c6cd83224d745b9b3ddec5
CreRecombinase <nwknoblauch@gmail.com>
2019-01-21 12:47:49 -0600

    fixed linking stuff

--- R/zzz.R
+++ R/zzz.R
@@ -9,12 +9,16 @@
 #' @examples
 #' pkgconfig("PKG_C_LIBS")
 #' @export
-pkgconfig <- function(opt = c("PKG_C_LIBS")) {
+pkgconfig <- function(opt = c("PKG_C_LIBS","PKG_RPATH")) {
 
     path <- Sys.getenv(
         x = "RZSTDLIB_RPATH",
         unset = system.file("libs", package="rzstdlib", mustWork=TRUE)
     )
+    if(opt=="PKG_RPATH"){
+      cat(paste0("-L",path," -lzstd -Wl,-rpath,",path))
+    }
+    else{
 
     if (nzchar(.Platform$r_arch)) {
         arch <- sprintf("/%s", .Platform$r_arch)
@@ -24,29 +28,42 @@ pkgconfig <- function(opt = c("PKG_C_LIBS")) {
     patharch <- paste0(path, arch)
 
     result <- switch(match.arg(opt),
-                    # PKG_CPPFLAGS = {
-                    #     sprintf('-I"%s"', system.file("include", package="Rhdf5lib"))
-                    # },
+                     # PKG_CPPFLAGS = {
+                     #     sprintf('-I"%s"', system.file("include", package="Rhdf5lib"))
+                     # },
                      PKG_C_LIBS = {
-                         switch(Sys.info()['sysname'],
-                                Windows = {
-                                    patharch <- gsub(x = shortPathName(patharch),
-                                                   pattern = "\\",
-                                                   replacement = "/",
-                                                   fixed = TRUE)
-                                    sprintf('-L%s -lzstd',
-                                            patharch)
-                                }, {
-                                    sprintf('-L%s -lzstd',
-                                            patharch)
-#                                    sprintf('%s/libhdf5.a %s/libsz.a -lz',
- #                                           patharch, patharch)
-                                }
-                         )
+                       switch(Sys.info()['sysname'],
+                              Windows = {
+                                patharch <- gsub(x = shortPathName(patharch),
+                                                 pattern = "\\",
+                                                 replacement = "/",
+                                                 fixed = TRUE)
+                                sprintf('-L%s -lzstd',
+                                        patharch)
+                              }, {
+                                sprintf('-L%s -lzstd',
+                                        patharch)
+                                #                                    sprintf('%s/libhdf5.a %s/libsz.a -lz',
+                                #                                           patharch, patharch)
+                              }
+                       )
+                     },
+                     PKG_RPATH = {
+                       switch(Sys.info()['sysname'],
+                              Windows = {
+                               stop("Can't set rpath on windows...")
+                              }, {
+                                sprintf('-rpath,%s/libzstd.so',
+                                        patharch)
+                                #                                    sprintf('%s/libhdf5.a %s/libsz.a -lz',
+                                #                                           patharch, patharch)
+                              }
+                       )
                      }
     )
 
     cat(result)
+    }
 }
 
 
