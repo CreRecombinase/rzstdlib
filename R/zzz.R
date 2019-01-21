55a1fdf8fa3057826773889362ccdd30370378d1
CreRecombinase <nwknoblauch@gmail.com>
2019-01-21 10:50:37 -0600

    first commit

---
+++ R/zzz.R
@@ -0,0 +1,52 @@
+#' Compiler arguments for using Rzstdlib
+#'
+#' This function returns values for
+#' \code{PKG_CC_FLAGS} variable for use in Makevars files.
+#'
+#' @param opt A scalar character from the list of available options;
+#' default is \code{PKG_C_LIBS}.
+#' @return \code{NULL}; prints the corresponding value to stdout.
+#' @examples
+#' pkgconfig("PKG_C_LIBS")
+#' @export
+pkgconfig <- function(opt = c("PKG_C_LIBS")) {
+
+    path <- Sys.getenv(
+        x = "RZSTDLIB_RPATH",
+        unset = system.file("libs", package="rzstdlib", mustWork=TRUE)
+    )
+
+    if (nzchar(.Platform$r_arch)) {
+        arch <- sprintf("/%s", .Platform$r_arch)
+    } else {
+        arch <- ""
+    }
+    patharch <- paste0(path, arch)
+
+    result <- switch(match.arg(opt),
+                    # PKG_CPPFLAGS = {
+                    #     sprintf('-I"%s"', system.file("include", package="Rhdf5lib"))
+                    # },
+                     PKG_C_LIBS = {
+                         switch(Sys.info()['sysname'],
+                                Windows = {
+                                    patharch <- gsub(x = shortPathName(patharch),
+                                                   pattern = "\\",
+                                                   replacement = "/",
+                                                   fixed = TRUE)
+                                    sprintf('-L%s -lzstd',
+                                            patharch)
+                                }, {
+                                    sprintf('-L%s -lzstd',
+                                            patharch)
+#                                    sprintf('%s/libhdf5.a %s/libsz.a -lz',
+ #                                           patharch, patharch)
+                                }
+                         )
+                     }
+    )
+
+    cat(result)
+}
+
+
